cfa6b3726aa1206fdb78dd8fd0b24bca0bc836dd
Pete Mason <pete.dev@robot9.uk>
2024-06-05 16:05:27 +0100

    Complete playGame functionality

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,9 +4,6 @@ const validChoices = ["rock", "paper", "scissors"];
 
 const numberRounds = 5;
 
-let computerScore = 0;
-let humanScore = 0;
-
 // Randomly pick between rock, paper, scissors
 const getComputerChoice = () => validChoices[Math.floor(Math.random() * 3)];
 
@@ -14,44 +11,73 @@ const getComputerChoice = () => validChoices[Math.floor(Math.random() * 3)];
 const getHumanChoice = () =>
   prompt("What is your choice?").trim().toLowerCase();
 
-//play a round
-//return true if the round was valid
-//increment the human or computer score as appropriate
-// (a draw awards no points)
-const playRound = (humanChoice, computerChoice) => {
-  //FIRST check that the human choice is valid
-  if (!validChoices.includes(humanChoice)) {
-    console.log("You must choose either rock, paper or scissors!");
-    return false;
+//play the game
+const playGame = () => {
+  let computerScore = 0;
+  let humanScore = 0;
+
+  let roundNumber = 1;
+
+  //play the required number of rounds
+  while (roundNumber <= numberRounds) {
+    //increment the round number if the round was valid
+    roundNumber += playRound(getHumanChoice(), getComputerChoice()) ? 1 : 0;
   }
 
-  //capitalise the first letter of each choice for display purposes
-  const formattedHumanChoice =
-    humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-  const formattedComputerChoice =
-    computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+  //declare a winner
+  let gameMessage;
 
-  //determine the winner!
-  if (humanChoice === computerChoice) {
-    console.log(`Draw! We both chose ${formattedHumanChoice}!`);
+  if (humanScore === computerScore) {
+    gameMessage = "Draw!";
+  } else if (humanScore > computerScore) {
+    gameMessage = "You win!";
   } else {
-    let isHumanWin =
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rocks") ||
-      (humanChoice === "scissors" && computerChoice === "paper");
-
-    let gameMessage = isHumanWin
-      ? `You win! ${formattedHumanChoice} beats ${formattedComputerChoice}!`
-      : `You lose! ${formattedComputerChoice} beats ${formattedHumanChoice}!`;
-
-    console.log(gameMessage);
-
-    if (isHumanWin) {
-      humanScore += 1;
-    } else {
-      computerScore += 1;
-    }
+    gameMessage = "I win!";
   }
 
-  return true;
+  console.log(
+    `${gameMessage} You scored ${humanScore}, and I scored ${computerScore}.`
+  );
+
+  //play a round
+  //return true if the round was valid
+  //increment the human or computer score as appropriate
+  // (a draw awards no points)
+  function playRound(humanChoice, computerChoice) {
+    //FIRST check that the human choice is valid
+    if (!validChoices.includes(humanChoice)) {
+      console.log("You must choose either rock, paper or scissors!");
+      return false;
+    }
+
+    //capitalise the first letter of each choice for display purposes
+    const formattedHumanChoice =
+      humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    const formattedComputerChoice =
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+
+    //determine the winner!
+    if (humanChoice === computerChoice) {
+      console.log(`Draw! We both chose ${formattedHumanChoice}!`);
+    } else {
+      let isHumanWin =
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rocks") ||
+        (humanChoice === "scissors" && computerChoice === "paper");
+
+      let roundMessage = isHumanWin
+        ? `You win! ${formattedHumanChoice} beats ${formattedComputerChoice}!`
+        : `You lose! ${formattedComputerChoice} beats ${formattedHumanChoice}!`;
+
+      console.log(roundMessage);
+
+      if (isHumanWin) {
+        humanScore += 1;
+      } else {
+        computerScore += 1;
+      }
+    }
+
+    return true;
+  }
 };
